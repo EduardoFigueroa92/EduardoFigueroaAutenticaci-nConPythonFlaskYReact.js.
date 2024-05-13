396e6c48ec903b06396881d7d7efd9551b40772d
Eduardo Figueroa <156800901+EduardoFigueroa92@users.noreply.github.com>
2024-05-13 19:23:05 +0000

    vacio

--- src/api/routes.py
+++ src/api/routes.py
@@ -11,12 +11,17 @@ api = Blueprint('api', __name__)
 # Allow CORS requests to this API
 CORS(api)
 
-
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    request_data = request.get_json()
+    if not request_data or 'email' not in request_data or 'username' not in request_data or 'password' not in request_data:
+        raise APIException('Invalid request body', status_code=400)
 
-    return jsonify(response_body), 200
+    new_user = User(email=request_data["email"], username=request_data["username"], password=request_data["password"])
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify(new_user.serialize())
+
+
